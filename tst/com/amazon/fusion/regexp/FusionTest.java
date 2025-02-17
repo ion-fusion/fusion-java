c8b9a7a5de45c0f8d7974a9e017d0c27f8a6663c
Jonathan Little <rationull@gmail.com>
2025-02-16 19:00:41 -0800

    Export content approved for release (without history)

---
+++ tst/com/amazon/fusion/regexp/FusionTest.java
@@ -0,0 +1,10 @@
+// Copyright (c) 2014 Amazon.com, Inc.  All rights reserved.
+
+package com.amazon.fusion.regexp;
+
+import com.amazon.fusion.junit.ScriptRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ScriptRunner.class)
+public class FusionTest {
+}
