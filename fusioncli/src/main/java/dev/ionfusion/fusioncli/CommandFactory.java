f36c3670fa630fcfd9d78198f089eee807706ee0
Todd V. Jonker <todd@toddjonker.net>
2026-03-30 12:53:50 -0700

    Hoist execution logic out of `CommandFactory`. (#501)
      * `Cli` handles the simple, single-command case.
      * `SequenceCli` and `SequenceCommand` handle semicolon-separated commands.
      * `CommandFactory` extends `SequenceCli`.

--- fusioncli/src/main/java/dev/ionfusion/fusioncli/CommandFactory.java
+++ fusioncli/src/main/java/dev/ionfusion/fusioncli/CommandFactory.java
@@ -5,16 +5,13 @@ package dev.ionfusion.fusioncli;
 
 import static dev.ionfusion.fusioncli.framework.OptionParser.extractOptions;
 
-import dev.ionfusion.fusioncli.framework.Cli;
 import dev.ionfusion.fusioncli.framework.Command;
-import dev.ionfusion.fusioncli.framework.Executor;
+import dev.ionfusion.fusioncli.framework.SequenceCli;
 import dev.ionfusion.fusioncli.framework.UsageException;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
 
 class CommandFactory
-    extends Cli<GlobalOptions>
+    extends SequenceCli<GlobalOptions>
 {
     public static final String APP_NAME = "fusion";
 
@@ -30,6 +27,14 @@ class CommandFactory
 
         myStdout = context.stdout();
         myStderr = context.stderr();
+    }
+
+
+    @Override
+    protected String[] extractCommonOptions(String[] commandLine)
+        throws UsageException
+    {
+        return extractOptions(context(), commandLine, true);
     }
 
 
@@ -53,120 +58,18 @@ class CommandFactory
     }
 
 
-    /**
-     * Makes an {@link Executor} for a single command in the sequence.
-     *
-     * @param commandLine includes the leading command name.
-     *
-     * @return an {@link Executor} to execute the command; not null.
-     *
-     * @throws UsageException if there are
-     * command-line errors preventing the command from being used.
-     */
-    public Executor makeExecutor(Command  command,
-                                 String[] commandLine)
-        throws UsageException
+    @Override
+    protected int handleUsage(UsageException e)
     {
-        // Strip off the leading command name, leaving the options and args.
-        int argCount = commandLine.length - 1;
-        String[] args = new String[argCount];
-        System.arraycopy(commandLine, 1, args, 0, argCount);
-
-        Executor exec = command.makeExecutor(context(), args);
-        if (exec == null)
+        myStdout.flush();                // Avoid commingled console output.
+        myStderr.println();
+        String message = e.getMessage();
+        if (message != null)
         {
-            throw new UsageException(command, null);
-        }
-        return exec;
-    }
-
-
-    /**
-     * @return an error code, zero meaning success.
-     */
-    int executeCommandLine(String... commandLine)
-        throws Exception
-    {
-        try
-        {
-            commandLine = extractOptions(context(), commandLine, true);
-
-            // Eagerly parse all commands and their args so that any errors can
-            // be reported before executing anything.
-            List<Executor> execs = makeExecutors(commandLine);
-            for (Executor exec : execs)
-            {
-                int errorCode = exec.execute();
-                if (errorCode != 0)
-                {
-                    return errorCode;
-                }
-            }
-
-            return 0;
-        }
-        catch (UsageException e)
-        {
-            myStdout.flush();                // Avoid commingled console output.
+            myStderr.println(message);
             myStderr.println();
-            String message = e.getMessage();
-            if (message != null)
-            {
-                myStderr.println(message);
-                myStderr.println();
-            }
-            writeUsage(e.getCommand());
-            return USAGE_ERROR_CODE;
         }
-    }
-
-
-    private List<Executor> makeExecutors(String[] commandLine)
-        throws Exception
-    {
-        List<Executor> execs = new ArrayList<>();
-
-        int curStartPos = 0;
-        for (int i = 0; i < commandLine.length; i++)
-        {
-            if (";".equals(commandLine[i])) {
-                int len = i-curStartPos;
-                if (len > 0) {
-                    Executor exec = makeExecutor(commandLine, curStartPos, len);
-                    execs.add(exec);
-                }
-                curStartPos = i+1;
-            }
-        }
-
-        int len = commandLine.length-curStartPos;
-        Executor exec = makeExecutor(commandLine, curStartPos, len);
-        execs.add(exec);
-
-        return execs;
-    }
-
-
-    /**
-     * Parses a segment of the command line into its command, options, and
-     * arguments, producing an {@link Executor} that implements the command.
-     *
-     * @param start the position within {@code commandLine} at which to start
-     * the segment.
-     * @param len the number of words in the segment.
-     *
-     * @return an error code, zero meaning success.
-     */
-    private Executor makeExecutor(String[] commandLine,
-                                  int start,
-                                  int len)
-        throws Exception
-    {
-        String[] segment = new String[len];
-        System.arraycopy(commandLine, start, segment, 0, len);
-
-        Command command = context().commandSuite().matchCommand(segment);
-
-        return makeExecutor(command, segment);
+        writeUsage(e.getCommand());
+        return USAGE_ERROR_CODE;
     }
 }
