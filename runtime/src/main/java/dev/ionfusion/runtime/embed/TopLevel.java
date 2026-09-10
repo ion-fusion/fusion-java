75ffb6b4a9ed0e71f8a26c0f78ccb246a322c902
ddalaklidhs <dalaklidisdemetres@gmail.com>
2026-09-10 23:56:36 +0300

    Drop import of relocated FusionInterruptedException.

--- runtime/src/main/java/dev/ionfusion/runtime/embed/TopLevel.java
+++ runtime/src/main/java/dev/ionfusion/runtime/embed/TopLevel.java
@@ -7,7 +7,6 @@ import com.amazon.ion.IonReader;
 import com.amazon.ion.IonValue;
 import com.amazon.ion.IonWriter;
 import com.amazon.ion.ValueFactory;
-import dev.ionfusion.fusion.FusionInterruptedException;
 import dev.ionfusion.runtime.base.FusionException;
 import dev.ionfusion.runtime.base.ResourceDescriptor;
 import java.io.File;
